54e06118bc688ad869347bcfe66d81bdb6914f4a
ABK <ABK@DESKTOP-PTCS16K>
2024-12-24 15:38:37 +0300

    Add project files.

---
+++ salary/Models/salary.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace salary.Models
+{
+    public class Salary
+    {
+        public int Add(int x, int y)
+        { return + x + y; }
+
+        public int Add(int x, int y, int z)
+        {
+            return x + y + z;
+        }
+        public double Add(double x, double y, double z) { return x + y + z; }
+        public int Add(int salaryy, int workhours, int hourrate, int total)
+        {
+           
+            return  salaryy + (workhours * hourrate);
+          
+        }
+
+    }
+
+}
---
+++ salary/Program.cs
@@ -0,0 +1,46 @@
+﻿using salary.Models;
+Salary salary = new Salary();
+int x;
+int y;
+int z;
+double a;
+double b;
+double c;
+int salaryy;
+int workhours;
+int hoursrate;
+int total;
+
+
+Console.WriteLine("Enter number");
+x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter number");
+y = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(salary.Add(x, y));
+
+
+Console.WriteLine("Enter number");
+x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter number");
+y = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter number");
+z = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(salary.Add(x, y, z));
+
+
+Console.WriteLine("Enter number");
+a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter number");
+b = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter number");
+c = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine(salary.Add(x, y, z));
+
+Console.WriteLine("Enter number");
+salaryy = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter number");
+workhours = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter number");
+hoursrate = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(salary.Add(salaryy, workhours, hoursrate));
+
